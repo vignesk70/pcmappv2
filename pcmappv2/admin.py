2be69427e9c861d1dcc72332cf8ff4f210c81dff
vignes <vignes_k@yahoo.com>
2018-09-04 09:53:05 +0800

    fix for db commented admin.py

--- pcmappv2/admin.py
+++ pcmappv2/admin.py
@@ -1,23 +1,23 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Member,Car,Payment
-admin.AdminSite.site_header='Peugeot Club Administration'
-
-class PaymentInLine(admin.TabularInline):
-    model = Payment
-    extras = 1
-
-class CarInLine(admin.TabularInline):
-    model = Car
-    search_fields = ['car.car_reg_no']
-    choices =1
-
-class MemberAdmin(admin.ModelAdmin):
-    search_fields = ['member_name']
-    list_display = ('member_name','member_since','member_expiry_date','member_phone','owner')
-    inlines = [CarInLine,PaymentInLine]
-
-admin.site.register(Member,MemberAdmin)
-admin.site.register(Car)
-admin.site.register(Payment)
+# from django.contrib import admin
+#
+# # Register your models here.
+# from .models import Member,Car,Payment
+# admin.AdminSite.site_header='Peugeot Club Administration'
+#
+# class PaymentInLine(admin.TabularInline):
+#     model = Payment
+#     extras = 1
+#
+# class CarInLine(admin.TabularInline):
+#     model = Car
+#     search_fields = ['car.car_reg_no']
+#     choices =1
+#
+# class MemberAdmin(admin.ModelAdmin):
+#     search_fields = ['member_name']
+#     list_display = ('member_name','member_since','member_expiry_date','member_phone','owner')
+#     inlines = [CarInLine,PaymentInLine]
+#
+# admin.site.register(Member,MemberAdmin)
+# admin.site.register(Car)
+# admin.site.register(Payment)

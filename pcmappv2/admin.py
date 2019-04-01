c9f64f3176617fd141cf600448d1aad5fc11a87e
Kassim Abdullah <kassim.abdullah@funneltree.net>
2019-04-01 12:40:28 +0800

    added admin view change

--- pcmappv2/admin.py
+++ pcmappv2/admin.py
@@ -14,13 +14,13 @@ class CarInLine(admin.TabularInline):
     choices =1
 
 class MemberAdmin(admin.ModelAdmin):
-    search_fields = ['member_name']
-    list_display = ('member_name','member_since','member_expiry_date','member_phone','owner','member_status')
+    search_fields = ['member_name','member_address_state',]
+    list_display = ('member_name','member_since','member_expiry_date','member_phone','owner','member_address_state','member_status',)
     inlines = [CarInLine,PaymentInLine]
 
 class CarAdmin(admin.ModelAdmin):
     list_display=('car_reg_no','member_id','car_model')
-    search_fields=('member_id__member_name','car_reg_no')
+    search_fields=('member_id__member_name','car_reg_no',)
 
 admin.site.register(Member,MemberAdmin)
 admin.site.register(Car,CarAdmin)

1c9fa13339d04569ae9b3985ee99b157bb40603c
Kassim Abdullah <kassim.abdullah@funneltree.net>
2019-12-09 23:33:29 +0800

    Update tests.py
    updated test.py

--- pcmappv2/tests.py
+++ pcmappv2/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from django.test import Client
 # Create your tests here.
+
+class ProjectTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/pcmappv2/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_baseredirect(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,302)
+
+    def test_baseredirect_with_oldurl(self):
+        response=self.client.get('/pcmapp/')
+        self.assertEqual(response.status_code,302)
+
+class MemberRegistrationTests(TestCase):
+
+    def test_registration_pageload(self):
+        response = self.client.get('/pcmappv2/register/')
+        self.assertEqual(response.status_code,200)

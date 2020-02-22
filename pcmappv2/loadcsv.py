e06930cb9a8c49ebcaaead65b0ddf18c8e471a9e
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-02-22 20:06:25 +0800

    updated for dumping user data

--- pcmappv2/loadcsv.py
+++ pcmappv2/loadcsv.py
@@ -38,7 +38,7 @@ for mem in members:
 
 """
 code for mysqlbackup on python pythonanywhere
-mysqldump -u peugeotclubmalay -h peugeotclubmalaysia.mysql.pythonanywhere-services.com 'peugeotclubmalay$pcmapp'  > db-backup_1Apr2019.sql
+mysqldump -u peugeotclubmalay -h peugeotclubmalaysia.mysql.pythonanywhere-services.com 'peugeotclubmalay$pcmapp'  > db-backup_22Feb2020.sql
 
 SELECT m.member_name, m.member_address_state, m.member_expiry_date,c.car_reg_no FROM pcmappv2_member m, pcmappv2_car c where c.member_id_id=m.id and m.member_status=1 and m.member_address_state in ('PG','KH')
 
@@ -50,7 +50,23 @@ Payment.objects.values('payment_date__year','payment_date__month').annotate(sum 
 
 """
 
+'''
+To dump the data. Run in Pythonanywhere console 
 
+import csv 
+from pcmappv2.models import *
+
+fields = [f.name for f in Member._meta.fields]
+
+with open('dumpdata.csv', mode='w') as file:
+    filewrite = csv.writer(file , delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    filewrite.writerow(fields)
+    for row in Member.objects.values(*fields):
+        filewrite.writerow([row[field] for field in fields])
+
+
+
+'''
 #member_name,member_email,member_phone,member_since,member_birthdate,member_address_state,member_address_postcode,member_on_chat,member_source,member_expiry_date
 #car_reg_no,car_model,car_engine_chasis,car_primary_sec,car_status
 #payment_date,payment_amount,payment_type

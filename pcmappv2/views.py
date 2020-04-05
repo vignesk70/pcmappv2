c479ac2f74027197d841658123b7032397fd6581
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-04-05 11:06:40 +0800

    Updated views For sending name with the email for renewal

--- pcmappv2/views.py
+++ pcmappv2/views.py
@@ -42,11 +42,11 @@ def email_renew(request):
 
 def email_renewal_notice(member):
     subject = "Your membership is(was) due on "+ (member.member_expiry_date).strftime("%d %b %Y")
-    message = subject + ". Please make payment and upload the receipt image from the Member Area of the PCM Website. "
+    message = "Dear "+member.member_name+",\n" +subject + ". Please make payment and upload the receipt image from the Member Area of the PCM Website. "
     email_from = settings.EMAIL_HOST_USER
     recipient_list = ['info@peugeotclubmalaysia.com',member.member_email]
     send_mail(subject,message,email_from,recipient_list)
-    # print(message," ",recipent_list)
+    #print(message," ",recipient_list)
     return True
 
 class IndexView(generic.TemplateView):

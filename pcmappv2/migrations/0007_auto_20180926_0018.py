b19b3b259084d35ea56f4773a4bcc9011b321ced
vignesk70 <vignes_k@yahoo.com>
2020-04-06 08:49:25 +0000

    synced

--- pcmappv2/migrations/0007_auto_20180926_0018.py
+++ pcmappv2/migrations/0007_auto_20180926_0018.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        
+       ('pcmappv2','0006_auto_20180912_0154'), 
     ]
 
     operations = [

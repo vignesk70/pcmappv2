4b6de942f8c88c4d8c56439e632c05310a9d885b
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-05-08 20:22:57 +0800

    updated admin for import export

--- pcmappv2/admin.py
+++ pcmappv2/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from datetime import date
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 
 # Register your models here.
 from .models import Member, Car, Payment, Activity
@@ -18,19 +18,19 @@ class CarInLine(admin.TabularInline):
     choices = 1
 
 
-class MemberAdmin(admin.ModelAdmin):
+class MemberAdmin(ImportExportMixin, admin.ModelAdmin):
     search_fields = ['member_name', 'member_address_state', ]
     list_display = ('member_name', 'member_since', 'member_expiry_date',
                     'member_phone', 'owner', 'member_address_state', 'member_status',)
     inlines = [CarInLine, PaymentInLine]
 
 
-class CarAdmin(admin.ModelAdmin):
+class CarAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('car_reg_no', 'member_id', 'car_model')
     search_fields = ('member_id__member_name', 'car_reg_no',)
 
 
-class PaymentAdmin(admin.ModelAdmin):
+class PaymentAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ['payment_car_reg_no', 'payment_date',
                     'payment_amount', 'payment_updated_date', 'payment_updated_by']
 
@@ -40,19 +40,7 @@ class PaymentAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-# admin.site.register(Member,MemberAdmin)
-#min.site.register(Car, CarAdmin)
-#admin.site.register(Payment, PaymentAdmin)
-# admin.site.register(Payment)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Activity)
-@admin.register(Member, Payment, Car)
-class MemberAdmin(ImportExportModelAdmin):
-    pass
-
-
-class PaymentAdmin(ImportExportModelAdmin):
-    pass
-
-
-class CarAdmin(ImportExportModelAdmin):
-    pass

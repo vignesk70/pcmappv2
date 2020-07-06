ce0fbf37d909246ce6dbf9a6ba7f5a6156c18bc5
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-07-06 19:18:22 +0800

    updted for issue related to two owner.

--- pcmappv2/forms.py
+++ pcmappv2/forms.py
@@ -2,60 +2,72 @@ from django import forms
 from django.forms.models import inlineformset_factory
 from .models import Member, Car, Payment
 
+
 class NewMemberRegistrationForm(forms.ModelForm):
     class Meta:
-        model=Member
-        fields = ['member_name','member_email','member_phone','member_birthdate','member_address_state','member_address_postcode','member_on_chat','member_source','member_pdpa_accepted']
+        model = Member
+        fields = ['member_name', 'member_email', 'member_phone', 'member_birthdate', 'member_address_state',
+                  'member_address_postcode', 'member_on_chat', 'member_source', 'member_pdpa_accepted']
         widgets = {
-            'member_birthdate': forms.DateInput(attrs={'type': 'date', 'format' :'YYYY-MM-DD'}),
-            'member_name' : forms.TextInput(attrs={'size':50}),
-            'member_email' : forms.TextInput(attrs={'size':50}),
-            'member_address_postcode' : forms.TextInput(attrs={'size':5}),
-            'member_on_chat' : forms.NullBooleanSelect(attrs=None),
-            'member_pdpa_accepted':forms.NullBooleanSelect(attrs=None),
+            'member_birthdate': forms.DateInput(attrs={'type': 'date', 'format': 'YYYY-MM-DD'}),
+            'member_name': forms.TextInput(attrs={'size': 50}),
+            'member_email': forms.TextInput(attrs={'size': 50}),
+            'member_address_postcode': forms.TextInput(attrs={'size': 5}),
+            'member_on_chat': forms.NullBooleanSelect(attrs=None),
+            'member_pdpa_accepted': forms.NullBooleanSelect(attrs=None),
         }
 
-CarRegistrationFormSet =  inlineformset_factory(Member,
-    Car,
-    fields = ['member_id','car_reg_no','car_model','car_primary_sec'],
-    can_delete = False,
-    extra=1)
+
+CarRegistrationFormSet = inlineformset_factory(Member,
+                                               Car,
+                                               fields=[
+                                                   'member_id', 'car_reg_no', 'car_model', 'car_primary_sec'],
+                                               can_delete=False,
+                                               extra=1)
 
 PaymentFormSet = inlineformset_factory(Member,
-    Payment,
-    fields = ['payment_date','payment_amount','payment_type','payment_receipt_image'],
-    extra=1,
-    can_delete = False,
-    widgets = {'payment_date':forms.DateInput(attrs={'type': 'date', 'format' :'YYYY-MM-DD'})})
+                                       Payment,
+                                       fields=['payment_date', 'payment_amount',
+                                               'payment_type', 'payment_receipt_image'],
+                                       extra=1,
+                                       can_delete=False,
+                                       widgets={'payment_date': forms.DateInput(attrs={'type': 'date', 'format': 'YYYY-MM-DD'})})
+
 
 class SCCheckForm(forms.Form):
-    car_reg_no = forms.CharField(label='Car Registration Number', max_length=10)
+    car_reg_no = forms.CharField(
+        label='Car Registration Number', max_length=10)
 
     def clean_car_reg_no(self):
         car_reg_no = self.cleaned_data['car_reg_no']
-        car_reg_no = car_reg_no.replace(" ","")
+        car_reg_no = car_reg_no.replace(" ", "")
         try:
-            obj = Car.objects.get(car_reg_no=car_reg_no)
+            obj = Car.objects.filter(car_reg_no=car_reg_no)
             return car_reg_no
         except Car.DoesNotExist:
             raise forms.ValidationError('No record found')
 
+
 class MembershipRenewForm(forms.ModelForm):
     class Meta:
-        model=Payment
-        fields = ['payment_car_reg_no','payment_date','payment_amount','payment_type','payment_receipt_image']
-        widgets = {'payment_date':forms.DateInput(attrs={'type': 'date', 'format' :'YYYY-MM-DD'})}
-        exclude=['payment_car_reg_no']
+        model = Payment
+        fields = ['payment_car_reg_no', 'payment_date',
+                  'payment_amount', 'payment_type', 'payment_receipt_image']
+        widgets = {'payment_date': forms.DateInput(
+            attrs={'type': 'date', 'format': 'YYYY-MM-DD'})}
+        exclude = ['payment_car_reg_no']
+
 
 class EditMemberRegistrationForm(forms.ModelForm):
     class Meta:
-        model=Member
-        fields = ['member_name','member_email','member_phone','member_birthdate','member_address_state','member_address_postcode','member_on_chat','member_source','member_pdpa_accepted']
+        model = Member
+        fields = ['member_name', 'member_email', 'member_phone', 'member_birthdate', 'member_address_state',
+                  'member_address_postcode', 'member_on_chat', 'member_source', 'member_pdpa_accepted']
         widgets = {
-            'member_birthdate': forms.DateInput(attrs={'type': 'date', 'format' :'YYYY-MM-DD'}),
-            'member_name' : forms.TextInput(attrs={'size':50,'readonly':'True'}),
-            'member_email' : forms.TextInput(attrs={'size':50, 'readonly':'True'}),
-            'member_address_postcode' : forms.TextInput(attrs={'size':5}),
-            'member_on_chat' : forms.NullBooleanSelect(attrs=None),
-            'member_pdpa_accepted':forms.NullBooleanSelect(attrs=None),
+            'member_birthdate': forms.DateInput(attrs={'type': 'date', 'format': 'YYYY-MM-DD'}),
+            'member_name': forms.TextInput(attrs={'size': 50, 'readonly': 'True'}),
+            'member_email': forms.TextInput(attrs={'size': 50, 'readonly': 'True'}),
+            'member_address_postcode': forms.TextInput(attrs={'size': 5}),
+            'member_on_chat': forms.NullBooleanSelect(attrs=None),
+            'member_pdpa_accepted': forms.NullBooleanSelect(attrs=None),
         }
--- pcmappv2/views.py
+++ pcmappv2/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect,HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
-from .forms import NewMemberRegistrationForm, CarRegistrationFormSet, PaymentFormSet,SCCheckForm, MembershipRenewForm, EditMemberRegistrationForm
+from .forms import NewMemberRegistrationForm, CarRegistrationFormSet, PaymentFormSet, SCCheckForm, MembershipRenewForm, EditMemberRegistrationForm
 from django.core.files.storage import FileSystemStorage
-from .models import Member,Payment,Car, Activity
+from .models import Member, Payment, Car, Activity
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
@@ -12,7 +12,7 @@ from datetime import date
 from django.utils.formats import date_format
 from django.core.mail import send_mail
 from django.conf import settings
-from django.urls import reverse,reverse_lazy
+from django.urls import reverse, reverse_lazy
 import json
 import urllib
 import requests
@@ -25,38 +25,50 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 
-def email(memname,paymdate):
-    subject = 'New membership request from '+ memname
-    message = subject + '. Payment date: '+ date_format(paymdate) +'. Please login to the admin site to validate' # reverse('pcmappv2:sccheck_detail',args=[str(request.pk)])
+
+def email(memname, paymdate):
+    subject = 'New membership request from ' + memname
+    # reverse('pcmappv2:sccheck_detail',args=[str(request.pk)])
+    message = subject + '. Payment date: ' + \
+        date_format(paymdate) + '. Please login to the admin site to validate'
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['info@peugeotclubmalaysia.com',]
-    send_mail(subject,message,email_from,recipient_list)
+    recipient_list = ['info@peugeotclubmalaysia.com', ]
+    send_mail(subject, message, email_from, recipient_list)
     return True
+
 
 def email_renew(request):
-    subject = 'Membership renewal from '+ request.payment_car_reg_no.member_name
-    message = subject + '. Payment date: '+ date_format(request.payment_date) +'. Please login to the admin site to validate' # reverse('pcmappv2:sccheck_detail',args=[str(request.pk)])
+    subject = 'Membership renewal from ' + request.payment_car_reg_no.member_name
+    # reverse('pcmappv2:sccheck_detail',args=[str(request.pk)])
+    message = subject + '. Payment date: ' + \
+        date_format(request.payment_date) + \
+        '. Please login to the admin site to validate'
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['info@peugeotclubmalaysia.com',]
-    send_mail(subject,message,email_from,recipient_list)
+    recipient_list = ['info@peugeotclubmalaysia.com', ]
+    send_mail(subject, message, email_from, recipient_list)
     return True
 
+
 def email_renewal_notice(member):
-    subject = "Your membership is(was) due on "+ (member.member_expiry_date).strftime("%d %b %Y")
-    message = render_to_string('pcmappv2/reminder.html',{'member':member})
+    subject = "Your membership is(was) due on " + \
+        (member.member_expiry_date).strftime("%d %b %Y")
+    message = render_to_string('pcmappv2/reminder.html', {'member': member})
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['info@peugeotclubmalaysia.com',member.member_email]
+    recipient_list = ['info@peugeotclubmalaysia.com', member.member_email]
     #recipient_list = [member.member_email]
-    send_mail(subject,message,email_from,recipient_list,html_message=message)
+    send_mail(subject, message, email_from,
+              recipient_list, html_message=message)
     #print(message," ",recipient_list)
     return True
 
+
 class IndexView(generic.TemplateView):
-    template_name='pcmappv2/index.html'
+    template_name = 'pcmappv2/index.html'
+
 
 class RegisterMember(generic.FormView):
-    template_name='pcmappv2/registermember.html'
-    form_class=NewMemberRegistrationForm
+    template_name = 'pcmappv2/registermember.html'
+    form_class = NewMemberRegistrationForm
     success_url = 'success'
 
     def get(self, request, *args, **kwargs):
@@ -84,7 +96,8 @@ class RegisterMember(generic.FormView):
             'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
             'response': recaptcha_response
         }
-        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+        r = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify', data=data)
         result = r.json()
 
         ''' End reCAPTCHA validation '''
@@ -100,14 +113,14 @@ class RegisterMember(generic.FormView):
         car_form.save()
         receipt_form.instance = self.object
         receipt_form.save()
-        memname=form.cleaned_data['member_name']
+        memname = form.cleaned_data['member_name']
         paymdate = receipt_form[0].cleaned_data['payment_date']
-        #print(memname,paymdate)
-        email(memname,paymdate)
+        # print(memname,paymdate)
+        email(memname, paymdate)
 
         return HttpResponseRedirect(self.get_success_url())
 
-    def form_invalid(self, form, car_form,receipt_form):
+    def form_invalid(self, form, car_form, receipt_form):
         return self.render_to_response(
             self.get_context_data(form=form,
                                   car_form=car_form,
@@ -115,18 +128,19 @@ class RegisterMember(generic.FormView):
 
 
 class NewMemberRegistationSuccess(generic.TemplateView):
-    template_name='pcmappv2/registrationsuccess.html'
+    template_name = 'pcmappv2/registrationsuccess.html'
 
 
 class ActivitiesList(generic.ListView):
-    template_name='pcmappv2/activities.html'
+    template_name = 'pcmappv2/activities.html'
     model = Activity
 
 
-class SCCheck(LoginRequiredMixin,generic.FormView):
-    template_name='pcmappv2/sccheck.html'
-    form_class=SCCheckForm
-    success_url='detail'
+class SCCheck(LoginRequiredMixin, generic.FormView):
+    template_name = 'pcmappv2/sccheck.html'
+    form_class = SCCheckForm
+    success_url = 'detail'
+
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
@@ -137,68 +151,79 @@ class SCCheck(LoginRequiredMixin,generic.FormView):
         else:
             return self.form_invalid(form)
 
-
-    def form_valid(self,form):
-        carid = Car.objects.get(car_reg_no=form.cleaned_data['car_reg_no'])
+    def form_valid(self, form):
+        carid = Car.objects.filter(
+            car_reg_no=form.cleaned_data['car_reg_no']).latest('id')
         self.success_url = 'detail/%s' % carid.pk
         return HttpResponseRedirect(self.get_success_url())
 
-    def form_invalid(self,form):
+    def form_invalid(self, form):
         return self.render_to_response(
             self.get_context_data(form=form))
 
 
-class SCcheckDetailView(LoginRequiredMixin,generic.DetailView):
+class SCcheckDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
     template_name = 'pcmappv2/sccheck_detail.html'
+
     def get_context_data(self, **kwargs):
-        context =  super(SCcheckDetailView, self).get_context_data(**kwargs)
-        context['car'] = Car.objects.get(pk=self.kwargs.get('pk',None))
+        context = super(SCcheckDetailView, self).get_context_data(**kwargs)
+        try:
+            pass
+        except expression as identifier:
+            pass
+        context['car'] = Car.objects.get(pk=self.kwargs.get('pk', None))
         context['member'] = Member.objects.get(id=context['car'].member_id.pk)
         return context
 
 
-class MemberArea(LoginRequiredMixin,generic.TemplateView):
-    template_name='pcmappv2/member_area.html'
-    model=Member
+class MemberArea(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'pcmappv2/member_area.html'
+    model = Member
+
     def get_context_data(self, **kwargs):
-        context =  super(MemberArea, self).get_context_data(**kwargs)
-        owner= self.request.user
-        context['member'] = get_object_or_404(Member,owner=self.request.user)
-        context['payment'] = Payment.objects.filter(payment_car_reg_no__owner=self.request.user)
-        context['cars'] = Car.objects.filter(member_id=get_object_or_404(Member,owner=self.request.user))
+        context = super(MemberArea, self).get_context_data(**kwargs)
+        owner = self.request.user
+        context['member'] = get_object_or_404(Member, owner=self.request.user)
+        context['payment'] = Payment.objects.filter(
+            payment_car_reg_no__owner=self.request.user)
+        context['cars'] = Car.objects.filter(
+            member_id=get_object_or_404(Member, owner=self.request.user))
         return context
 
-class PCMMemberExpiring(LoginRequiredMixin,generic.ListView):
-    template_name='pcmappv2/member_expiring.html'
-    model=Member
+
+class PCMMemberExpiring(LoginRequiredMixin, generic.ListView):
+    template_name = 'pcmappv2/member_expiring.html'
+    model = Member
     #queryset = Member.objects.select_related().filter(member_expiry_date__month=date.today().month,member_expiry_date__year=date.today().year)
-    q1 = Member.objects.select_related().filter(member_expiry_date__year=date.today().year,member_expiry_date__month=date.today().month,member_status=True).order_by('member_expiry_date')
-    q2 = Member.objects.select_related().filter(member_expiry_date__lte=date.today(),member_status=True).order_by('member_expiry_date')
+    q1 = Member.objects.select_related().filter(member_expiry_date__year=date.today().year,
+                                                member_expiry_date__month=date.today().month, member_status=True).order_by('member_expiry_date')
+    q2 = Member.objects.select_related().filter(member_expiry_date__lte=date.today(),
+                                                member_status=True).order_by('member_expiry_date')
     #queryset = Member.objects.select_related().filter(member_expiry_date__year=date.today().year,member_expiry_date__month=date.today().month,member_status=True).order_by('member_expiry_date')
-    queryset = q1|q2
+    queryset = q1 | q2
 
 
-class MembershipRenew(LoginRequiredMixin,generic.FormView):
+class MembershipRenew(LoginRequiredMixin, generic.FormView):
     template_name = 'pcmappv2/renewmember.html'
-    model =  Payment
+    model = Payment
     form_class = MembershipRenewForm
     success_url = 'success'
 
-
     def get_context_data(self, **kwargs):
-        context =  super(MembershipRenew, self).get_context_data(**kwargs)
-        context['member'] = get_object_or_404(Member,owner=self.request.user)
+        context = super(MembershipRenew, self).get_context_data(**kwargs)
+        context['member'] = get_object_or_404(Member, owner=self.request.user)
         return context
 
     def get(self, request, *args, **kwargs):
-        initial = {'payment_car_reg_no': get_object_or_404(Member,owner=self.request.user)}
+        initial = {'payment_car_reg_no': get_object_or_404(
+            Member, owner=self.request.user)}
         self.object = None
         form_class = self.get_form_class()
         form = self.form_class(initial=self.initial)
         #form = self.get_form(form_class)
         #form.instance.payment_car_reg_no = get_object_or_404(Member,owner=self.request.user)
-        #form..payment_car_reg_no=get_object_or_404(Member,owner=self.request.user)
+        # form..payment_car_reg_no=get_object_or_404(Member,owner=self.request.user)
         return self.render_to_response(
             self.get_context_data(form=form))
 
@@ -207,26 +232,28 @@ class MembershipRenew(LoginRequiredMixin,generic.FormView):
         form_class = self.get_form_class()
         #form = self.get_form(form_class)
         form = MembershipRenewForm(self.request.POST, self.request.FILES)
-        #form['payment_car_reg_no']=self.request.user
+        # form['payment_car_reg_no']=self.request.user
         ''' Begin reCAPTCHA validation '''
         recaptcha_response = request.POST.get('g-recaptcha-response')
         data = {
             'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
             'response': recaptcha_response
         }
-        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+        r = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify', data=data)
         result = r.json()
 
         ''' End reCAPTCHA validation '''
 
-        if (form.is_valid() and result['success'] ):
+        if (form.is_valid() and result['success']):
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
 
     def form_valid(self, form):
-        payment= form.save(commit=False)
-        payment.payment_car_reg_no = get_object_or_404(Member,owner=self.request.user)
+        payment = form.save(commit=False)
+        payment.payment_car_reg_no = get_object_or_404(
+            Member, owner=self.request.user)
         payment.save()
         #self.object = form.save()
         email_renew(payment)
@@ -236,24 +263,25 @@ class MembershipRenew(LoginRequiredMixin,generic.FormView):
         return self.render_to_response(
             self.get_context_data(form=form))
 
-class RenewSuccess(generic.TemplateView):
-    template_name='pcmappv2/renewsuccess.html'
 
-class MembershipEdit(LoginRequiredMixin,generic.UpdateView):
+class RenewSuccess(generic.TemplateView):
+    template_name = 'pcmappv2/renewsuccess.html'
+
+
+class MembershipEdit(LoginRequiredMixin, generic.UpdateView):
     template_name = 'pcmappv2/member_area_edit.html'
-    model =  Member
+    model = Member
     form_class = EditMemberRegistrationForm
     success_url = 'pcmappv2:member_area'
 
-
     def get_context_data(self, **kwargs):
-        context =  super(MembershipEdit, self).get_context_data(**kwargs)
-        context['member'] = get_object_or_404(Member,owner=self.request.user)
+        context = super(MembershipEdit, self).get_context_data(**kwargs)
+        context['member'] = get_object_or_404(Member, owner=self.request.user)
         print(context)
         return context
 
     def get(self, request, *args, **kwargs):
-        self.object = get_object_or_404(Member,pk=self.kwargs['pk'])
+        self.object = get_object_or_404(Member, pk=self.kwargs['pk'])
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         return self.render_to_response(
@@ -264,17 +292,22 @@ class MembershipEdit(LoginRequiredMixin,generic.UpdateView):
 
 
 class StatView(generic.TemplateView):
-    template_name='pcmappv2/statistics.html'
+    template_name = 'pcmappv2/statistics.html'
+
 
 class LineChartJSONView(BaseLineChartView):
-    allcars = Car.objects.all().annotate(total=Count('car_model')).order_by('car_model')
+    allcars = Car.objects.all().annotate(
+        total=Count('car_model')).order_by('car_model')
+
     def get_labels(self):
         """Return 7 labels for the x-axis."""
-        allcars = Car.objects.all().order_by('car_model').values('car_model').distinct().annotate(total=Count('car_model'))
-        label=[]
+        allcars = Car.objects.all().order_by('car_model').values(
+            'car_model').distinct().annotate(total=Count('car_model'))
+        label = []
         for x in allcars:
             label.append(x['car_model'])
-        return label #["January 18", "February", "March", "April", "May", "June", "July"]
+        # ["January 18", "February", "March", "April", "May", "June", "July"]
+        return label
 
     def get_providers(self):
         """Return names of datasets."""
@@ -282,13 +315,15 @@ class LineChartJSONView(BaseLineChartView):
 
     def get_data(self):
         """Return 3 datasets to plot."""
-        allcars = Car.objects.all().order_by('car_model').values('car_model').distinct().annotate(total=Count('car_model'))
-        data=[]
+        allcars = Car.objects.all().order_by('car_model').values(
+            'car_model').distinct().annotate(total=Count('car_model'))
+        data = []
         for x in allcars:
             data.append(x['total'])
-        return [data] #[[75, 44, 92, 11, 44, 95, 35],
-                #[41, 92, 18, 3, 73, 87, 92],
-                #[87, 21, 94, 3, 90, 13, 65]]
+        return [data]  # [[75, 44, 92, 11, 44, 95, 35],
+        #[41, 92, 18, 3, 73, 87, 92],
+        # [87, 21, 94, 3, 90, 13, 65]]
+
     def get_colors(self):
         """Return a new shuffle list of color so we change the color
         each time."""
@@ -297,7 +332,8 @@ class LineChartJSONView(BaseLineChartView):
         return next_color(colors)
 
 
-line_chart = generic.TemplateView.as_view(template_name='pcmappv2/line_chart.html')
+line_chart = generic.TemplateView.as_view(
+    template_name='pcmappv2/line_chart.html')
 line_chart_json = LineChartJSONView.as_view()
 
 # class BootStrapView(generic.FormView):
